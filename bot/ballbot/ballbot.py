9719d6378b35007ba107e4d52c5a0da23a623019
abbenson <abbenson@ncsu.edu>
2016-03-01 11:30:51 -0500

    Added some initial velocity control functionality. It is disabled by default and requires a mode change from the controller.

--- bot/ballbot/ballbot.py
+++ bot/ballbot/ballbot.py
@@ -1,5 +1,6 @@
 import re
 import time
+import math
 import pyDMCC
 import Adafruit_BBIO.UART as UART
 import serial
@@ -12,6 +13,11 @@ cape0mtr1 = 1
 cape0mtr2 = 1
 cape1mtr1 = -1
 cape1mtr2 = -1
+
+motor_forward_max = 375
+motor_forward_min = 180
+motor_reverse_max = -375
+motor_reverse_min = -180
 
 # Setup serial connection to IMU
 ser = serial.Serial(port="/dev/ttyO1", baudrate=57600)
@@ -32,7 +38,25 @@ pitch_motor_right.velocity_pid = constants
 roll_motor_left.velocity_pid = constants
 roll_motor_right.velocity_pid = constants
 
+motor_time = 0
+pitch_left_vel = 0
+pitch_right_vel = 0
+roll_left_vel = 0
+roll_right_vel = 0
+R = 1
+
 print "starting loop"
+
+
+def get_velocities(p_l, p_r, r_l, r_r):
+    v_x = .5 * (p_l + p_r)
+    v_y = .5 * (r_l + r_r)
+    w_x = v_x / R
+    w_y = v_y / R
+    v_x *= math.cos(w_x)
+    v_y *= math.cos(w_y)
+    return v_x, v_y
+
 
 while 1:
     try:
@@ -49,25 +73,43 @@ while 1:
         roll_new = float(ser_str[2])
         ser.close()
 
-        # Update PID
-        bot_controller.update(0, 0, pitch_new, roll_new)
-        pitch_corr = int(bot_controller.pitch_pid.output)
-        roll_corr = int(bot_controller.roll_pid.output)
+        # Update Relative Velocities
+        v_x_new, v_y_new = get_velocities(pitch_motor_left.velocity, pitch_motor_right.velocity,
+                                          roll_motor_left.velocity,
+                                          roll_motor_right.velocity)
 
-        # if (time.time()*1000 - last_mot_time) >= 250:
+        # Update PID
+        bot_controller.update(v_x_new, v_y_new, pitch_new, roll_new)
+
+        pitch_corr = int(bot_controller.pitch_pid.output)
+        if pitch_corr > motor_forward_max:
+            pitch_corr = motor_forward_max
+        elif pitch_corr < motor_reverse_max:
+            pitch_corr = motor_reverse_max
+
+        roll_corr = int(bot_controller.roll_pid.output)
+        if roll_corr > motor_forward_max:
+            roll_corr = motor_forward_max
+        elif roll_corr < motor_reverse_max:
+            roll_corr = motor_reverse_max
+
+        print "\r Pitch Correction: %d;     Roll Correction: %d" % (pitch_corr, roll_corr),
+
+        # if (time.time()*1000 - motor_time) >= 750:
         # Set motor velocities
-        print "p: %d" % pitch_corr
-        print "r: %d" % roll_corr
+        # print "motor set"
         pitch_motor_left.velocity = pitch_corr * cape0mtr1
         pitch_motor_right.velocity = pitch_corr * cape1mtr1
         roll_motor_left.velocity = roll_corr * cape0mtr2
         roll_motor_right.velocity = roll_corr * cape1mtr2
+
+        motor_time = time.time() * 1000
 
     except KeyboardInterrupt:
         pitch_motor_left.velocity = 0
         pitch_motor_right.velocity = 0
         roll_motor_left.velocity = 0
         roll_motor_right.velocity = 0
-        ser.close();
+        ser.close()
         print"closing"
         exit()
--- bot/ballbot/controller.py
+++ bot/ballbot/controller.py
@@ -3,10 +3,10 @@ from PID import PID
 
 class Controller:
     def __init__(self):
-        self.vx_pid = PID(10, 0, 0)
-        self.vy_pid = PID(10, 0, 0)
-        self.pitch_pid = PID(1, 0, 0)
-        self.roll_pid = PID(1, 0, 0)
+        self.vx_pid = PID(1, 0, 0)
+        self.vy_pid = PID(1, 0, 0)
+        self.pitch_pid = PID(10, 0, 0)
+        self.roll_pid = PID(10, 0, 0)
         self.pitch = 0
         self.roll = 0
         self.vx = 0
@@ -15,9 +15,20 @@ class Controller:
         self.vy_pid.SetPoint = 0
         self.pitch_pid.SetPoint = 0
         self.roll_pid.SetPoint = 0
+        self.mode = 'pitch'
 
     def update(self, vx_new, vy_new, pitch_new, roll_new):
-        self.vx_pid.update(vx_new)
-        self.vy_pid.update(vy_new)
-        self.pitch_pid.update(pitch_new)
-        self.roll_pid.update(roll_new)
+        if self.mode == 'pitch':
+            self.pitch_pid.update(pitch_new)
+            self.roll_pid.update(roll_new)
+        if self.mode == 'velocity':
+            self.vx_pid.update(vx_new)
+            self.vy_pid.update(vy_new)
+            self.pitch_pid.SetPoint = self.vx_pid.output
+            self.roll_pid.SetPoint = self.vy_pid.output
+            self.pitch_pid.update(pitch_new)
+            self.roll_pid.update(roll_new)
+
+    def change_mode(self, new_mode):
+        assert isinstance(new_mode, object)
+        self.mode = new_mode
